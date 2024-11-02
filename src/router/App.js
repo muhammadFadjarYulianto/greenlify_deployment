29052d54b39e079c0eaa1e3a9ec323c51efdebdf
Damar Galih <damarmuhamad13@gmail.com>
2024-11-02 12:08:14 +0700

    ins(trashure): initialitation trashure project

---
+++ src/router/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
