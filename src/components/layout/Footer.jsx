1856b4ffc2b3b0b8c4ef78860945221dd2f147da
Damar Galih <damarmuhamad13@gmail.com>
2024-11-19 17:49:15 +0700

    feat(home): slicing home page from figma

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -1,7 +1,102 @@
+import { Link } from "react-router-dom";
+import Logo from "@/assets/images/logo.svg";
+
 export default function Footer() {
 	return (
-		<footer className="text-black">
-			<p>Footer</p>
+		<footer className="w-full h-[480px] bg-emerald-600 flex flex-col items-center justify-between py-10 mt-[66px] relative">
+			<div className="w-11/12 max-w-[1200px] flex flex-col lg:flex-row justify-between gap-10">
+				<div className="flex flex-col items-start gap-4">
+					<div className="flex items-center gap-4">
+						<img src={Logo} alt="Logo" className="w-full h-full object-cover" />
+						<span className="text-[#fdfaf1] text-lg font-medium">
+							Transformasikan sampah menjadi manfaat
+						</span>
+					</div>
+					<div className="flex gap-4"></div>
+				</div>
+
+				<div className="flex flex-col gap-5">
+					<h4 className="text-[#fdfaf1] text-xl font-bold">Navigation</h4>
+					<Link to="/" className="text-[#fdfaf1] text-base font-medium">
+						Beranda
+					</Link>
+					<Link
+						to="/statistik"
+						className="text-[#fdfaf1] text-base font-medium"
+					>
+						Statistik
+					</Link>
+					<Link to="/prediksi" className="text-[#fdfaf1] text-base font-medium">
+						Prediksi
+					</Link>
+					<Link to="/produk" className="text-[#fdfaf1] text-base font-medium">
+						Produk
+					</Link>
+				</div>
+
+				{/* Community */}
+				<div className="flex flex-col gap-5">
+					<h4 className="text-[#fdfaf1] text-xl font-bold">Community</h4>
+					<Link
+						to="/tentang-kami"
+						className="text-[#fdfaf1] text-base font-medium"
+					>
+						Tentang Kami
+					</Link>
+				</div>
+
+				{/* Social Links */}
+				<div className="flex flex-col gap-5">
+					<h4 className="text-[#fdfaf1] text-xl font-bold">Socials</h4>
+					<a
+						href="https://discord.com/"
+						className="text-[#fdfaf1] text-base font-medium"
+					>
+						Discord
+					</a>
+					<a
+						href="https://www.instagram.com/"
+						className="text-[#fdfaf1] text-base font-medium"
+					>
+						Instagram
+					</a>
+					<a
+						href="https://x.com/"
+						className="text-[#fdfaf1] text-base font-medium"
+					>
+						Twitter
+					</a>
+					<a
+						href="https://www.facebook.com/"
+						className="text-[#fdfaf1] text-base font-medium"
+					>
+						Facebook
+					</a>
+				</div>
+			</div>
+
+			<div className="w-11/12 border-t-2 border-[#fdfaf1]" />
+
+			{/* Bottom Section */}
+			<div className="w-11/12 max-w-[1200px] flex flex-col lg:flex-row items-center justify-between gap-4">
+				<div className="text-[#fdfaf1] text-base font-medium">
+					©2024 ErsTalent. All rights reserved
+				</div>
+				<div className="flex gap-4">
+					<Link
+						to="/privacy-policy"
+						className="text-[#fdfaf1] text-base font-medium"
+					>
+						Privacy & Policy
+					</Link>
+					<Link
+						to="/terms-condition"
+						className="text-[#fdfaf1] text-base font-medium"
+					>
+						Terms & Condition
+					</Link>
+				</div>
+			</div>
 		</footer>
 	);
 }
--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -9,31 +9,56 @@ import {
 
 export default function Header() {
 	return (
-		<div className="w-full h-[75px] px-[65px] bg-[#fdfaf1] border-b border-slate-300 justify-between items-center inline-flex">
+		<div className="w-full h-[75px] px-[65px] border-b border-slate-300 justify-between items-center inline-flex">
 			<div className="flex items-center gap-2.5">
 				<Link to="/">
-					<img src={Logo} alt="Logo" className="h-10" />
+					<img src={Logo} alt="Logo" className="h-11" />
 				</Link>
 			</div>
-			<NavigationMenu className="hidden md:block">
-				<NavigationMenuList>
-					{["Beranda", "Statistik", "Prediksi", "Produk", "Tentang Kami"].map(
-						(item) => (
-							<NavigationMenuItem key={item}>
-								<Link
-									to={`/${item.toLowerCase().replace(" ", "")}`}
-									className="px-7 py-2 text-[#1b4332] text-base font-semibold font-roboto hover:bg-[#e9f5ee] rounded-md"
-								>
-									{item}
-								</Link>
-							</NavigationMenuItem>
-						),
-					)}
-				</NavigationMenuList>
-			</NavigationMenu>
-			<Button className="h-10 px-4 py-2 bg-[#40916c] text-white text-sm font-medium font-inter hover:bg-[#347a57]">
-				Klasifikasi
-			</Button>
+
+			<div className="hidden md:flex items-center gap-4">
+				<NavigationMenu className="hidden md:block">
+					<NavigationMenuList>
+						{["Beranda", "Statistik", "Prediksi", "Produk", "Tentang Kami"].map(
+							(item) => (
+								<NavigationMenuItem key={item}>
+									<Link
+										to={`/${item.toLowerCase().replace(" ", "")}`}
+										className="px-10 py-2 text-emerald-700 font-semibold hover:text-emerald-800"
+									>
+										{item}
+									</Link>
+								</NavigationMenuItem>
+							),
+						)}
+					</NavigationMenuList>
+				</NavigationMenu>
+			</div>
+
+			<div className="flex items-center gap-4">
+				<Button size="lg">Klasifikasi</Button>
+				<div className="md:hidden">
+					<Button className="p-2 bg-emerald-500 text-white">
+						<svg
+							aria-label="Toggle Menu"
+							role="img"
+							xmlns="http://www.w3.org/2000/svg"
+							className="h-6 w-6"
+							fill="none"
+							viewBox="0 0 24 24"
+							stroke="currentColor"
+						>
+							<title>Toggle Menu</title>
+							<path
+								strokeLinecap="round"
+								strokeLinejoin="round"
+								strokeWidth="2"
+								d="M4 6h16M4 12h16M4 18h16"
+							/>
+						</svg>
+					</Button>
+				</div>
+			</div>
 		</div>
 	);
 }
