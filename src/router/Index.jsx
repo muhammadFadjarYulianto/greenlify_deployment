8cb7b74df544aa02eb21c3e36f17e530f15a5ac4
Damar Galih <damarmuhamad13@gmail.com>
2024-11-21 14:54:10 +0700

    feat(produk): add produk page with responsive mode

--- src/router/Index.jsx
+++ src/router/Index.jsx
@@ -4,7 +4,7 @@ import Layout from "@/components/layout/Layout.jsx";
 import Home from "@/pages/Home.jsx";
 import Statistic from "@/pages/Statistic";
 import Prediction from "@/pages/Prediction";
-import Product from "@/pages/Product";
+import Products from "@/pages/Products.jsx";
 import About from "@/pages/About";
 import Errors from "@/pages/Errors";
 
@@ -16,7 +16,7 @@ export default function Index() {
 				<Route path="/beranda" element={<Navigate to="/" />} />
 				<Route path="/statistik" element={<Statistic />} />
 				<Route path="/prediksi" element={<Prediction />} />
-				<Route path="/produk" element={<Product />} />
+				<Route path="/produk" element={<Products />} />
 				<Route path="/tentangkami" element={<About />} />
 				<Route path={"*"} element={<Errors />} />
 			</Route>
