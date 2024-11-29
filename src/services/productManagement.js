969234f65949b38f2ba26b8c12b9eb79831da860
Damar Galih <damarmuhamad13@gmail.com>
2024-11-29 19:59:10 +0700

    fix(dashboardcategory): crud category

--- src/services/productManagement.js
+++ src/services/productManagement.js
@@ -1,10 +1,6 @@
 import axios from "axios";
 import { PRODUCT_MANAGEMENT_ENDPOINT } from "@/constants/routesAPI";
 
-/**
- * Mendapatkan data produk dari server.
- * @returns {Promise<object>} - Data produk dari API.
- */
 export async function getProductsManagement() {
     try {
         const token = localStorage.getItem("access_token");
@@ -13,19 +9,16 @@ export async function getProductsManagement() {
             throw new Error("Token tidak tersedia. Silakan login kembali.");
         }
 
-        // Tambahkan token ke header
         const response = await axios.get(PRODUCT_MANAGEMENT_ENDPOINT, {
             headers: {
                 Authorization: `Bearer ${token}`,
             },
         });
 
-        // Debugging: Log seluruh respons
         console.log("Full response:", response);
 
-        // Cek jika respons data ada dan mengandung properti data
         if (response.data && response.data.data) {
-            return response.data.data; // Kembalikan data produk
+            return response.data.data;
         } else {
             console.error("Data tidak ditemukan di respons API.");
             throw new Error("Data produk tidak ditemukan.");
@@ -35,3 +28,4 @@ export async function getProductsManagement() {
         throw new Error("Gagal mengambil data produk. Silakan coba lagi.");
     }
 }
+
