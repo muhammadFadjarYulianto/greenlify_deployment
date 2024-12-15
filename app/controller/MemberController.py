9586d806a795221c74a39a861edba0fbe39c17f9
fahmi-kartika <naufalfahmi26@gmail.com>
2024-12-15 14:13:08 +0700

    revisi inputan validasi rt rw pada controller member

--- app/controller/MemberController.py
+++ app/controller/MemberController.py
@@ -1,8 +1,31 @@
 from flask import request, jsonify
 from app import db, response
 from app.model.member import Member
+import re
 
 
+def formatArray(members):
+    return [satuMember(member) for member in members]
+
+def satuMember(member):
+    return {
+        'id': member.id,
+        'desa': member.desa,
+        'rw': member.rw,
+        'rt': member.rt,
+        'created_at': member.created_at,
+        'updated_at': member.updated_at
+    }
+
+def indexMember():
+    try:
+        members = Member.query.all()
+        data = formatArray(members)
+        return response.success(data)
+    except Exception as e:
+        print(e)
+        return response.serverError([], "Gagal mengambil data member")
+    
 def tambahMember():
     try:
         desa = request.form.get('desa') or request.json.get('desa')
@@ -16,13 +39,17 @@ def tambahMember():
 
         if not rw:
             return response.badRequest([], "Kolom rw wajib diisi.")
-        if len(rw) < 1 or len(rw) > 3:  
+        if len(rw) < 1 or len(rw) > 3:
             return response.badRequest([], "Kolom rw harus berupa string dengan panjang 1 hingga 3 karakter.")
+        if not re.match(r'^\d+$', rw):  
+            return response.badRequest([], "Kolom rw harus berupa angka.")
 
         if not rt:
             return response.badRequest([], "Kolom rt wajib diisi.")
-        if len(rt) < 1 or len(rt) > 3:  
+        if len(rt) < 1 or len(rt) > 3:
             return response.badRequest([], "Kolom rt harus berupa string dengan panjang 1 hingga 3 karakter.")
+        if not re.match(r'^\d+$', rt):  
+            return response.badRequest([], "Kolom rt harus berupa angka.")
 
         memberBaru = Member(
             desa=desa,
@@ -51,13 +78,17 @@ def memperbaruiMember(id):
             member.desa = desa
 
         if rw:
-            if len(rw) < 1 or len(rw) > 3:  
+            if len(rw) < 1 or len(rw) > 3:
                 return response.badRequest([], "Kolom rw harus berupa string dengan panjang 1 hingga 3 karakter.")
+            if not re.match(r'^\d+$', rw):  
+                return response.badRequest([], "Kolom rw harus berupa angka.")
             member.rw = rw
 
         if rt:
-            if len(rt) < 1 or len(rt) > 3:  
+            if len(rt) < 1 or len(rt) > 3:
                 return response.badRequest([], "Kolom rt harus berupa string dengan panjang 1 hingga 3 karakter.")
+            if not re.match(r'^\d+$', rt):  
+                return response.badRequest([], "Kolom rt harus berupa angka.")
             member.rt = rt
 
         db.session.commit()
