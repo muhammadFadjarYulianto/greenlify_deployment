6b7c8e7eb50a03ccb298d00f8c2ed4fb303efa11
Damar Galih <damarmuhamad13@gmail.com>
2024-12-16 17:53:27 +0700

    fix(deploy): fixing deploy github action io

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { defineConfig } from "vite"
 
 export default defineConfig({
   plugins: [react()],
-  base: "/greenlify/",
+  // base: "/greenlify/",
   // server: {
   //   proxy: {
   //     changeOrigin: true,
