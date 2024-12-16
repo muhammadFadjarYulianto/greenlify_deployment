784a67f4d7d4b8018ebca774da70bcfcd46f1a9e
Damar Galih <damarmuhamad13@gmail.com>
2024-12-17 01:15:04 +0700

    build: update build for deployment

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { HashRouter as Router } from "react-router-dom";
 import "@/styles/index.css";
 import App from "@/App.jsx";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
 	// <React.StrictMode>
-		<Router>
 			<App />
-		</Router>
 	// </React.StrictMode>,
 );
