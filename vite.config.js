ed8289f4a6bd110918d6b9045eda408b890aa7e9
Damar Galih <damarmuhamad13@gmail.com>
2024-12-16 17:34:46 +0700

    Add proxy and update endpoint

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,22 @@
 import path from "path"
 import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+import {defineConfig} from "vite"
 
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+    plugins: [react()],
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "./src"),
+        },
+        extensions: ['.js', '.jsx', '.ts', '.tsx']
     },
-    extensions: ['.js', '.jsx', '.ts', '.tsx']
-  },
+    server: {
+        proxy: {
+            "/api": {
+                target: "http://13.228.244.152",
+                changeOrigin: true,
+                rewrite: (path) => path.replace(/^\/api/, ""),
+            },
+        },
+    },
 })
